7440ad49730a5db61c38dfdd12ca3fa08c49bf48
babalinno <93177168+babalinno@users.noreply.github.com>
2026-08-24 00:15:47 -0500

    feat: crear clase Persona como padre de Cliente

---
+++ src/main/java/alvaro/taller1/biblioteca/git/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alvaro.taller1.biblioteca.git;
+
+/**
+ *
+ * @author ALVARO
+ */
+public class Persona {
+    String id;
+    String nombre;
+    String telefono;
+}
