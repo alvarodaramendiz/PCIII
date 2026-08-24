00b53e549f0e901cab36c43b75e9df71d9977d4c
babalinno <93177168+babalinno@users.noreply.github.com>
2026-08-24 00:29:17 -0500

    feat: implementar CREATE de cliente

--- src/main/java/alvaro/taller1/biblioteca/git/Main.java
+++ src/main/java/alvaro/taller1/biblioteca/git/Main.java
@@ -9,6 +9,27 @@ import java.util.Scanner;
 public class Main { 
     static ArrayList<Cliente> clientes = new ArrayList<>(); 
     static Scanner sc = new Scanner(System.in); 
+    
+    void crearCliente() {
+    System.out.println("\n--- CREAR CLIENTE ---");
+    System.out.print("Ingrese el ID del cliente: ");
+    String id = sc.nextLine();
+    sc.nextLine(); // Limpiar el búfer de entrada
+
+    System.out.print("Ingrese el nombre del cliente: ");
+    String nombre = sc.nextLine();
+
+    System.out.print("Ingrese el correo electrónico: ");
+    String email = sc.nextLine();
+    
+    System.out.print("Ingrese el telefono: ");
+    String telefono = sc.nextLine();
+
+    Cliente nuevoCliente = new Cliente(email, id, nombre, telefono);
+    clientes.add(nuevoCliente);
+
+    System.out.println("¡Cliente registrado exitosamente!");
+}
 
     public static void main(String[] args) { 
         // Aquí irá el menú (Fase 8) 
