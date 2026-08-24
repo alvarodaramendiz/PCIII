07a40a25bf26f703330b9526d5dfefab97648857
babalinno <93177168+babalinno@users.noreply.github.com>
2026-08-24 00:16:53 -0500

    feat: crear clase Cliente heredando de Persona

---
+++ src/main/java/alvaro/taller1/biblioteca/git/Cliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alvaro.taller1.biblioteca.git;
+
+/**
+ *
+ * @author ALVARO
+ */
+public class Cliente extends Persona {
+    
+    String email;
+    
+}
